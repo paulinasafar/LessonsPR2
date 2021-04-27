3579e6410ff4ce8e6ae28d732085b7393284ceae
Paulina Safar <paulina.safar@yahoo.com>
2021-04-27 17:31:52 +0200

    Uebungsbeispiele01

--- src/Uebungsbeispiele01/Order.java
+++ src/Uebungsbeispiele01/Order.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 public class Order {
     private int nrArticles;
     private int capacityOrder;
-    private Article[] articles;
+    private Article[] order;
 
-    public Order(int nrArticles, int capacityOrder) {
+    public Order(int capacityOrder) {
         this.nrArticles = 0;
         this.capacityOrder = capacityOrder;
-        this.articles = new Article[capacityOrder];
+        this.order = new Article[capacityOrder];
     }
 
     public int getNrArticles() {
@@ -30,32 +30,72 @@ public class Order {
     }
 
     public Article[] getArticles() {
-        return articles;
+        return order;
     }
 
     public void setArticles(Article[] articles) {
-        this.articles = articles;
+        this.order = articles;
     }
 
-
-
-    public double findMostExpensiveArticle(Article[] articles) {
-        double highestPrice = articles[0].getPrice();
-
-        for (int i = 0; i < articles.length-1; i++) {
-            if(articles[i].getPrice() < articles[i+1].getPrice()) {
-                highestPrice = articles[i+1].getPrice();
+    public void addArticle(Article a) {
+        for (int i = 0; i < order.length; i++) {
+            if (order[i] != String){
+                order[i] = a;
             }
         }
-        return highestPrice + 1;}
+    }
 
+    public int findMostExpensiveArticle(Article[] articles) {
+        int highestPrice = 0;
+
+        for (int i = 0; i < articles.length - 1; i++) {
+            if (articles[i].getPrice() < articles[i + 1].getPrice()) {
+                highestPrice = i + 1;
+            }
+        }
+        return highestPrice + 1;
+    }
+
+    public int findMostExpensiveOrderPosition(Article[] order) {
+        int mostExpensiveOrder = 0;
+
+        for (int i = 0; i < order.length - 1; i++) {
+            if (order[i].getPrice() * order[i].getNumber() < order[i + 1].getPrice() * order[i + 1].getNumber()) {
+                mostExpensiveOrder = i + 1;
+            }
+        }
+        return mostExpensiveOrder + 1;
+    }
+
+    public double sumOrder(Article[] order) {
+        double sum = 0.0;
+
+        for (int i = 0; i < order.length; i++) {
+            sum += (order[i].getPrice() * order[i].getNumber());
+        }
+        return sum;
+    }
+
+    public double calculateTax(Article[] order){
+        double tax1 = 0.0;
+        double totalTax = 0.0;
+        for (int i = 0; i < order.length; i++) {
+            if(order[i].getPrice() < 20) {
+                tax1 = (order[i].getPrice() * 0.05) * order[i].getNumber();
+                totalTax += tax1;
+            }else {
+                tax1 = (order[i].getPrice() * 0.20) * order[i].getNumber();
+                totalTax += tax1;
+            }
+        }
+    return totalTax;}
+
+    public double sumOrderWithTax(Article[] order) {
+        double sumOrderWithTax = sumOrder(order) + calculateTax(order);
+    return sumOrderWithTax;}
 
     @Override
     public String toString() {
-        return "Order{" +
-                "nrArticles=" + nrArticles +
-                ", capacityOrder=" + capacityOrder +
-                ", articles=" + Arrays.toString(articles) +
-                '}';
+        return "Order: " + Arrays.toString(order);
     }
 }
--- src/Uebungsbeispiele01/OrderApp.java
+++ src/Uebungsbeispiele01/OrderApp.java
@@ -1,29 +1,48 @@
 package Uebungsbeispiele01;
 
+import java.util.Arrays;
+
 public class OrderApp {
 
     public static void main(String[] args) {
 
         Article a = new Article("Orange", 1, 2.22);
         Article b = new Article("Banana", 2, 1.59);
-        Article c = new Article("Kiwi", 3, 2.12);
+        Article c = new Article("Kiwi", 3, 4.12);
         Article d = new Article("Apple", 4, 1.28);
         Article e = new Article("Physallis", 5, 2.68);
-        Article f = new Article("Ananas", 6, 2.98);
-        Article g = new Article("Erdbeere", 7, 3.85);
-        Article h = new Article("Brot", 8, 4.23);
-        Article i = new Article("Milch", 9, 1.18);
-        Article j = new Article("Käse", 10, 5.15);
+        Article f = new Article("Ananas", 6, 8.98);
+        Article g = new Article("Strawberry", 7, 3.85);
+        Article h = new Article("Bread", 8, 4.23);
+        Article i = new Article("Milk", 9, 3.18);
+        Article j = new Article("Cheese", 10, 7.94);
+        Article k = new Article("Torte", 11, 25.15);
+        Article l = new Article("Meat", 12, 42.52);
+        Article m = new Article("Wine", 13, 22.56);
+        Article n = new Article("Steak", 10, 71.61);
 
         System.out.println(a.getName());
-        a.setName("Himbeere");
+        a.setName("Blueberry");
+        System.out.println(a.getName());
+        System.out.println("-------------------------------------------------------");
 
-        Order o1 = new Order(5, 5);
-        Order o2 = new Order(2, 6);
-        Order o3 = new Order(6, 10);
+        Order o1 = new Order(5);
+        Order o2 = new Order(3);
+        Order o3 = new Order(6);
 
+        o1.addArticle(h);
+        o1.addArticle(m);
+        o1.addArticle(a);
+        o1.addArticle(c);
+        o1.addArticle(l);
 
+        o2.addArticle(m);
+        o2.addArticle(b);
+        o2.addArticle(e);
 
+        System.out.println(o1);
+        System.out.println();
+        System.out.println(o2);
 
     }
 }
